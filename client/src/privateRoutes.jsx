0f0e399f0f117a80eb4ce3b6dd8c96cdd6687b15
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-11-28 18:34:16 +0200

    Add role-based private routes and recruiter profile
    This commit introduces a differentiation of private routes based on user roles. Instead of having one PrivateRoute component, there are two separate components CPrivateRoutes and RPrivateRoutes to handle route protection for candidate and recruiter roles respectively. This allows to have more control and customize the behaviour based on the user role.
    
    This commit also includes creation of a new page RecruiterProfile. The profile page layout was previously blindly reusing CandidateProfile layout regardless of the role which was not suitable for recruiter role.

--- client/src/privateRoutes.jsx
+++ client/src/privateRoutes.jsx
@@ -1,8 +1,38 @@
 import {useSelector} from "react-redux";
 import {Navigate, Outlet} from "react-router-dom";
-import {SIGNIN} from "./util/routes.js";
+import {HOME, SIGNIN} from "./util/routes.js";
+import {parseJwt} from "./util/jwt.js";
+import CandidateProfile from "./components/Pages/Profile/CandidateProfile.jsx";
+import RecruiterProfile from "./components/Pages/Profile/RecruiterProfile.jsx";
 
-export const PrivateRoutes = () => {
-    const user = useSelector(state => state.auth.user)
-    return user ? <Outlet /> : <Navigate to={SIGNIN} />
+export const CPrivateRoutes = () => {
+    const token = useSelector(state => state.auth.accessToken)
+    if(!token) {
+        return <Navigate to={SIGNIN} />
+    }
+    return parseJwt(token).role === "candidate" ? <Outlet /> : <Navigate to={HOME} />
+}
+
+export const RPrivateRoutes = () => {
+    const token = useSelector(state => state.auth.accessToken)
+    if(!token) {
+        return <Navigate to={SIGNIN} />
+    }
+    return parseJwt(token).role === "recruiter" ? <Outlet /> : <Navigate to={HOME} />
+}
+
+export const ProfileRoute = () => {
+    const token = useSelector(state => state.auth.accessToken);
+    if (!token) {
+        return <Navigate to={SIGNIN} />;
+    }
+
+    const role = parseJwt(token).role;
+    if (role === "candidate") {
+        return <CandidateProfile />;
+    } else if (role === "recruiter") {
+        return <RecruiterProfile />;
+    } else {
+        return <Navigate to={HOME} />;
+    }
 }
