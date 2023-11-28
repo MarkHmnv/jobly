bfb78985e7bb488935f496cf915789c589761865
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-11-28 18:32:09 +0200

    Add handling of concurrent token refresh attempts
    Mutex mechanism has been added to ensure that if multiple components request a new access token at the same time, they will have to wait for the ongoing refresh token request to finish before they proceed. Package 'async-mutex' was added to the project to manage the concurrency.

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,10 +1,11 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {BASE_URL, REFRESH_TOKEN} from "../../util/constants";
 import {removeCredentials, resetCredentials} from "./authSlice.js";
+import {Mutex} from "async-mutex";
 
 const baseQuery = fetchBaseQuery({
     baseUrl: BASE_URL,
-    prepareHeaders: (headers, { getState }) => {
+    prepareHeaders: (headers, {getState}) => {
         const token = getState().auth.accessToken;
         if (token) {
             headers.set('Authorization', `Bearer ${token}`);
@@ -13,30 +14,49 @@ const baseQuery = fetchBaseQuery({
     },
 })
 
+const mutex = new Mutex();
+
 const baseQueryWithReauth = async (args, api, extraOptions) => {
-    const result = await baseQuery(args, api, extraOptions)
+    await mutex.waitForUnlock();
+    let result = await baseQuery(args, api, extraOptions);
 
     if (result.error && result.error.status === 401) {
-        const refreshResult = await fetch(REFRESH_TOKEN, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ token: localStorage.getItem('refreshToken') })
-        })
+        if (!mutex.isLocked()) {
+            const release = await mutex.acquire();
+            const refreshToken = api.getState().auth.refreshToken;
 
-        if (refreshResult.ok) {
-            const refreshTokenResult = await refreshResult.json()
-            api.dispatch(resetCredentials(refreshTokenResult))
-            return baseQuery(args, api, extraOptions)
+            try {
+                const refreshResult = await baseQuery(
+                    {
+                        url: REFRESH_TOKEN,
+                        method: 'POST',
+                        body: {refresh: refreshToken},
+                    },
+                    api,
+                    extraOptions,
+                );
+
+                if (refreshResult.data) {
+                    api.dispatch(resetCredentials(refreshResult.data));
+                    result = await baseQuery(args, api, extraOptions);
+                } else {
+                    console.log('refresh error', refreshResult.error);
+                    api.dispatch(removeCredentials());
+                }
+            } finally {
+                release();
+            }
         } else {
-            api.dispatch(removeCredentials())
+            await mutex.waitForUnlock();
+            result = await baseQuery(args, api, extraOptions);
         }
     }
 
-    return result
-}
+    return result;
+};
 
 export const apiSlice = createApi({
-    baseQuery,
+    baseQuery: baseQueryWithReauth,
     tagTypes: ["Candidate", "Recruiter", "Vacancy"],
     endpoints: () => ({})
 })
