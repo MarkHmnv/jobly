5df194fdfbb5130788d698162d37661a4b9001fe
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-12-03 15:04:20 +0200

    Update recruiter model and add new views and endpoints
    Altered fields in the 'recruiter' model to allow 'about', 'city', and 'country' attributes to have null values. Added ListAPIView views for 'Category' and 'Skill' models and associated them with respective URL endpoints. Integrated 'getAllCandidates' query in the frontend 'userSlice'.

---
+++ api/core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import CategoryListView, SkillListView
+
+urlpatterns = [
+    path('categories/', CategoryListView.as_view(), name='category-list'),
+    path('skills/', SkillListView.as_view(), name='skill-list')
+]
--- api/core/views.py
+++ api/core/views.py
@@ -1,7 +1,26 @@
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from core.serializers import TokenWithRoleSerializer
+from core.models import Category, Skill
+from core.serializers import TokenWithRoleSerializer, CategorySerializer, SkillSerializer
 
 
 class TokenWithRoleView(TokenObtainPairView):
     serializer_class = TokenWithRoleSerializer
+
+
+class BasicListView(generics.ListAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class CategoryListView(BasicListView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class SkillListView(BasicListView):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
