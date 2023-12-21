eb4fe81ca5818706f11d80f2c13f3ebd5bcec4c4
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-12-21 19:27:00 +0200

    Update vacancy page, add ability to apply for vacancy and applications list

--- client/src/components/Pages/Vacancy/Vacancy.jsx
+++ client/src/components/Pages/Vacancy/Vacancy.jsx
@@ -1,11 +1,19 @@
 import {useEffect, useState} from "react";
-import {useGetVacancyQuery} from "../../../redux/slices/vacancySlice.js";
+import {useApplyForVacancyMutation, useGetVacancyQuery} from "../../../redux/slices/vacancySlice.js";
 import {useParams} from "react-router-dom";
 import Loader from "../../Loader/Loader.jsx";
+import {isCandidate} from "../../../util/jwt.js";
+import {useSelector} from "react-redux";
+import {toast} from "react-toastify";
+import {toastError} from "../../../util/toastUtil.jsx";
 
 const Vacancy = () => {
     const {id} = useParams()
     const {data: vacancy, isLoading} = useGetVacancyQuery(id)
+
+    const token = useSelector(state => state.auth.accessToken);
+    const isUserCandidate = isCandidate(token);
+    const [applyForVacancy, {isLoading: isApplying}] = useApplyForVacancyMutation();
 
     const [title, setTitle] = useState("");
     const [category, setCategory] = useState("");
@@ -15,6 +23,9 @@ const Vacancy = () => {
     const [country, setCountry] = useState("");
     const [city, setCity] = useState("");
     const [description, setDescription] = useState("");
+
+    const [coverLetter, setCoverLetter] = useState("");
+    const [applications, setApplications] = useState([]);
 
     useEffect(() => {
         if (vacancy) {
@@ -26,49 +37,123 @@ const Vacancy = () => {
             setCountry(vacancy.country)
             setCity(vacancy.city)
             setDescription(vacancy.description)
+            setApplications(vacancy.applications)
         }
     }, [vacancy, isLoading]);
 
+    const handleApply = () => {
+        try {
+            applyForVacancy({
+                coverLetter: {
+                    "cover_letter": coverLetter
+                }, id: id
+            });
+            toast.success("Successfully applied");
+        } catch (error) {
+            console.log(error)
+            toastError(error);
+        }
+    }
+
     return (
         isLoading ? <Loader/> :
-            <div className=" mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div className="px-4 sm:px-0">
-                    <h3 className="text-base font-semibold leading-7 text-gray-900">{title}</h3>
-                    <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500"></p>
+            <main className="p-6">
+                <div className="grid lg:grid-cols-2 gap-6">
+                    <div>
+                        <div className="bg-white border border-gray-200 shadow-lg">
+                            <div className="p-4 border-b border-gray-200">
+                                <h2 className="text-2xl font-bold text-black">{title}</h2>
+                            </div>
+                            <div className="p-4 space-y-4 text-black">
+                                <h3 className="text-xl font-semibold">Category</h3>
+                                <p className="text-base">{category}</p>
+                                <h3 className="text-xl font-semibold">Skills</h3>
+                                <p className="text-base">{skills}</p>
+                                <h3 className="text-xl font-semibold">Experience (in years)</h3>
+                                <p className="text-base">{experience}</p>
+                                <h3 className="text-xl font-semibold">Salary</h3>
+                                <p className="text-base">{salary ? `$${salary}` : "Negotiable"}</p>
+                                <h3 className="text-xl font-semibold">Location</h3>
+                                <p className="text-base">{country}, {city}</p>
+                                <h3 className="text-xl font-semibold">Description</h3>
+                                <p className="text-base">{description}</p>
+                            </div>
+                        </div>
+                    </div>
+                    {isUserCandidate &&
+                        <div>
+                            <div className="bg-white border border-gray-200 shadow-lg">
+                                <div className="p-4 border-b border-gray-200">
+                                    <h2 className="text-2xl font-bold text-black">Apply for this Job</h2>
+                                </div>
+                                <div className="p-4 space-y-4 text-black">
+                                    <div className="space-y-2">
+                                        <label className="text-lg font-bold text-gray-700" htmlFor="cover-letter">
+                                            Cover Letter
+                                        </label>
+                                        <input className="w-full p-2 border border-gray-300 rounded" id="cover-letter"
+                                               value={coverLetter}
+                                               onChange={e => setCoverLetter(e.target.value)}
+                                               required/>
+                                    </div>
+                                    <button
+                                        className="w-full px-4 py-2 mt-4 text-white bg-blue-500 rounded hover:bg-blue-600"
+                                        onClick={handleApply}
+                                    >
+                                        Apply Now
+                                    </button>
+                                    {isApplying && <Loader/>}
+                                </div>
+                            </div>
+                        </div>
+                    }
                 </div>
-                <div className="mt-6 border-t border-gray-100">
-                    <dl className="divide-y divide-gray-100">
-                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                            <dt className="text-sm font-medium leading-6 text-gray-900">Category</dt>
-                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{category}</dd>
+                {applications && applications.length > 0 && (
+                    <div className="hidden lg:block mt-10">
+                        <div className="bg-white border border-gray-200 shadow-lg">
+                            <div className="p-4 border-b border-gray-200">
+                                <h2 className="text-2xl font-bold text-black">Applications</h2>
+                            </div>
+                            <div className="p-4 text-black">
+                                <ul className="space-y-4">
+                                    {applications.map((application) => (
+                                        <li key={application.id} className="flex items-center space-x-4">
+                                            <img
+                                                alt="Avatar"
+                                                className="w-10 h-10 rounded-full"
+                                                height="40"
+                                                src="/placeholder.svg"
+                                                style={{
+                                                    aspectRatio: "40/40",
+                                                    objectFit: "cover",
+                                                }}
+                                                width="40"
+                                            />
+                                            <div className="flex flex-col">
+                                                <h3 className="text-lg font-bold">
+                                                    {application.candidate.user.first_name}{' '}
+                                                    {application.candidate.user.last_name}
+                                                </h3>
+                                                <span
+                                                    className="mt-2 px-2 py-1 text-sm text-white bg-green-500 rounded">
+                                                    {application.candidate.position || 'Position not specified'}
+                                                </span>
+                                                <span className="text-sm">
+                                                    <strong>Skills:</strong>{' '}
+                                                    {application.candidate.skills
+                                                        .map((skill) => skill.name)
+                                                        .join(', ')}
+                                                </span>
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
                         </div>
-                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                            <dt className="text-sm font-medium leading-6 text-gray-900">Skills</dt>
-                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{skills}</dd>
-                        </div>
-                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                            <dt className="text-sm font-medium leading-6 text-gray-900">Experience</dt>
-                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{experience}</dd>
-                        </div>
-                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                            <dt className="text-sm font-medium leading-6 text-gray-900">Salary</dt>
-                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{salary}</dd>
-                        </div>
-                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                            <dt className="text-sm font-medium leading-6 text-gray-900">Country</dt>
-                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{country}</dd>
-                        </div>
-                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                            <dt className="text-sm font-medium leading-6 text-gray-900">City</dt>
-                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{city}</dd>
-                        </div>
-                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                            <dt className="text-sm font-medium leading-6 text-gray-900">Description</dt>
-                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{description}</dd>
-                        </div>
-                    </dl>
-                </div>
-            </div>
+                    </div>
+                )}
+
+            </main>
     );
 };
 
