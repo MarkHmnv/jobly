4fb028973cf4dd35707073d767dd2337bb8cc7f9
Mark <skrytyjinkognito54@gmail.com>
2024-03-08 00:26:17 +0200

    Added .env to the frontend

--- client/src/util/constants.js
+++ client/src/util/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000/api"
+export const BASE_URL = import.meta.env.VITE_BASE_URL
 export const CANDIDATES_URL = "/candidates/"
 export const RECRUITERS_URL = "/recruiters/"
 export const GET_ACCESS_TOKEN = "/token/"
