85a48199cf8061053c173e70ebe9d1118a39ad75
Mark <skrytyjinkognito54@gmail.com>
2024-04-17 16:10:38 +0300

    Fixed parsing of errors that come from the backend

--- client/src/util/toastUtil.jsx
+++ client/src/util/toastUtil.jsx
@@ -1,14 +1,22 @@
-import { toast } from 'react-toastify';
+import {toast} from 'react-toastify';
 
 export const toastError = (error) => {
     let messages = [];
-    if (error.data && error.data.user && error.data.user.email) {
-        error.data.user.email.forEach(message => {
-            messages.push(message);
-        });
+    if (error.data) {
+        extractMessages(error.data, messages);
     } else {
-        messages.push(error.data?.message || error.error);
+        messages.push(error.message || "An unknown error occurred");
     }
 
     messages.forEach(msg => toast.error(msg));
-}
+};
+
+const extractMessages = (obj, messages = []) => {
+    if (typeof obj === 'string') {
+        messages.push(obj);
+    } else if (Array.isArray(obj)) {
+        obj.forEach(item => extractMessages(item, messages));
+    } else if (typeof obj === 'object' && obj !== null) {
+        Object.values(obj).forEach(value => extractMessages(value, messages));
+    }
+};
